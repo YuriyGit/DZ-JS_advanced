1912f3d26d03404a2090cc15c345778c104b4889
Yuriy <bomawo3052@zamaneta.com>
2024-07-11 10:08:10 +0400

    add folder 15-build

---
+++ 15-build/index.js
@@ -0,0 +1,10 @@
+'use strict';
+import {User} from "./user.js";
+import {Task} from "./task.js";
+
+
+const task = new Task();
+
+const user = new User(task);
+
+user.do()
---
+++ 15-build/user.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export class User {
+    task
+
+    constructor(task) {
+        this.task = task
+    }
+
+    do() {
+        this.task.run()
+    }
+}
+
