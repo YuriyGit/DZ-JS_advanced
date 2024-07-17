cc5f0950c9caf901cc6581f0ed4515b0eb45542b
Yuriy <bomawo3052@zamaneta.com>
2024-07-17 20:28:07 +0400

    снова dice ...

--- 2-dice/app.js
+++ 2-dice/app.js
@@ -1,18 +1,22 @@
-'use strict'
+'use strict';
 
 function getDiceNumber(typeDice = 'd12') {
-    const diceNumber = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20'];
-    let maxValueDice = '';
-    if (!diceNumber.find(el => el === typeDice)) {
-        console.log('Не вверно введён тип dice');
-        return null
+    const diceNumber =
+        {
+            d4: 4,
+            d6: 6,
+            d8: 8,
+            d10: 10,
+            d12: 12,
+            d16: 16,
+            d20: 20,
+        };
+
+    if (typeDice in diceNumber) {
+        console.log(Math.floor(Math.random() * diceNumber[typeDice]) + 1);
+        return;
     }
-    for (let el of typeDice) {
-        if (!isNaN(Number(parseInt(el)))) {
-            maxValueDice += el
-        }
-    }
-    console.log(Math.floor(Math.random() * (maxValueDice - 1 + 1)) + 1);
+    console.log('Не вверно введён тип dice');
 }
 
 getDiceNumber('d10')
@@ -21,3 +25,4 @@ getDiceNumber('d15')
 getDiceNumber('d16')
 getDiceNumber('d4')
 getDiceNumber('d11')
+getDiceNumber(11)
